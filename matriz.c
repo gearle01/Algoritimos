28bca1783e6f6a6b9da54359e085836d7a085d13
gearle01 <47864743+gearle01@users.noreply.github.com>
2019-06-13 08:13:15 -0300

    Add files via upload

---
+++ matriz.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+
+void main(){
+	 int matriz[7][7]={0};
+	  
+	 matriz[0][3]=1;
+
+	 for(int l=1; l<4; l++){
+	 	 for(int c=1; c<7; c++){
+	 	 	 matriz[l][c] = matriz[l-1][c-1] + matriz[l-1][c+1];
+	 	     
+	 	  }	 	 
+	   } 
+	 	for(int l=0; l<7; l++){
+	 		for(int c=0; c<7; c++){
+	 			printf("%d  ", matriz[l][c]);
+	 		}
+	 		printf("\n");
+	 	}	
+}
